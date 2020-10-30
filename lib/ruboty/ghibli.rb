36fa26f071798052f8ce24fb2be433222e928e25
Takashi Kokubun <takashikkbn@gmail.com>
2020-10-29 20:40:12 -0700

    bundle gem ruboty-ghibli

---
+++ lib/ruboty/ghibli.rb
@@ -0,0 +1,8 @@
+require "ruboty/ghibli/version"
+
+module Ruboty
+  module Ghibli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/ruboty/ghibli/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module Ghibli
+    VERSION = "0.1.0"
+  end
+end
