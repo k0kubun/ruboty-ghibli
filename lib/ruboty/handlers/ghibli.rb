04b222688966bfec67b01d5224d7bd4db4f992e3
Takashi Kokubun <takashikkbn@gmail.com>
2020-10-29 22:08:01 -0700

    Fix a silly bug

--- lib/ruboty/handlers/ghibli.rb
+++ lib/ruboty/handlers/ghibli.rb
@@ -2,20 +2,20 @@ module Ruboty
   module Handlers
     class Ghibli < Base
       PICTURES = {
-        baron:        (1..50).map { |i| 'http://www.ghibli.jp/gallery/baron%3d.jpg' % i },
-        chihiro:      (1..50).map { |i| 'http://www.ghibli.jp/gallery/chihiro%3d.jpg' % i },
-        ged:          (1..50).map { |i| 'http://www.ghibli.jp/gallery/ged%3d.jpg' % i },
-        ghiblies:     (1..50).map { |i| 'http://www.ghibli.jp/gallery/ghiblies%3d.jpg' % i },
-        howl:         (1..50).map { |i| 'http://www.ghibli.jp/gallery/howl%3d.jpg' % i },
-        kaguyahime:   (1..50).map { |i| 'http://www.ghibli.jp/gallery/kaguyahime%3d.jpg' % i },
-        karigurashi:  (1..50).map { |i| 'http://www.ghibli.jp/gallery/karigurashi%3d.jpg' % i },
-        kazetachinu:  (1..50).map { |i| 'http://www.ghibli.jp/gallery/kazetachinu%3d.jpg' % i },
-        kokurikozaka: (1..50).map { |i| 'http://www.ghibli.jp/gallery/kokurikozaka%3d.jpg' % i },
-        marnie:       (1..50).map { |i| 'http://www.ghibli.jp/gallery/marnie%3d.jpg' % i },
-        mimi:         (1..50).map { |i| 'http://www.ghibli.jp/gallery/mimi%3d.jpg' % i },
-        mononoke:     (1..50).map { |i| 'http://www.ghibli.jp/gallery/mononoke%3d.jpg' % i },
-        ponyo:        (1..50).map { |i| 'http://www.ghibli.jp/gallery/ponyo%3d.jpg' % i },
-        yamada:       (1..50).map { |i| 'http://www.ghibli.jp/gallery/yamada%3d.jpg' % i },
+        baron:        (1..50).map { |i| 'http://www.ghibli.jp/gallery/baron%03d.jpg' % i },
+        chihiro:      (1..50).map { |i| 'http://www.ghibli.jp/gallery/chihiro%03d.jpg' % i },
+        ged:          (1..50).map { |i| 'http://www.ghibli.jp/gallery/ged%03d.jpg' % i },
+        ghiblies:     (1..50).map { |i| 'http://www.ghibli.jp/gallery/ghiblies%03d.jpg' % i },
+        howl:         (1..50).map { |i| 'http://www.ghibli.jp/gallery/howl%03d.jpg' % i },
+        kaguyahime:   (1..50).map { |i| 'http://www.ghibli.jp/gallery/kaguyahime%03d.jpg' % i },
+        karigurashi:  (1..50).map { |i| 'http://www.ghibli.jp/gallery/karigurashi%03d.jpg' % i },
+        kazetachinu:  (1..50).map { |i| 'http://www.ghibli.jp/gallery/kazetachinu%03d.jpg' % i },
+        kokurikozaka: (1..50).map { |i| 'http://www.ghibli.jp/gallery/kokurikozaka%03d.jpg' % i },
+        marnie:       (1..50).map { |i| 'http://www.ghibli.jp/gallery/marnie%03d.jpg' % i },
+        mimi:         (1..50).map { |i| 'http://www.ghibli.jp/gallery/mimi%03d.jpg' % i },
+        mononoke:     (1..50).map { |i| 'http://www.ghibli.jp/gallery/mononoke%03d.jpg' % i },
+        ponyo:        (1..50).map { |i| 'http://www.ghibli.jp/gallery/ponyo%03d.jpg' % i },
+        yamada:       (1..50).map { |i| 'http://www.ghibli.jp/gallery/yamada%03d.jpg' % i },
       }
 
       on /ghibli( +(?<title>.+))?/, name: 'ghibli', description: PICTURES.keys.join(' ')
